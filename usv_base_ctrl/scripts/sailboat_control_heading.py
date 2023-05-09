6727f1facf04a00538930cea92509186cad9d125
lucasmrdt <lucas.marandat@gmail.com>
2023-05-09 15:43:52 +0200

    add gym handler for sailboat_scenario3 scenario and speed up the simulation at maximum

--- usv_base_ctrl/scripts/sailboat_control_heading.py
+++ usv_base_ctrl/scripts/sailboat_control_heading.py
@@ -1,246 +1,219 @@
 #!/usr/bin/env python
-# license removed for brevity
 
 import rospy
+import zmq
+import msgpack
 import math
+import time
 import tf
+from nav_msgs.msg import Odometry
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Quaternion
-from geometry_msgs.msg import Twist, Point, Quaternion
-from std_msgs.msg import Float64
+from std_srvs.srv import Empty
+from gazebo_msgs.msg import ModelStates
+from wind_current.srv import GetSpeed, GetSpeedResponse
 
-initial_pose = Odometry()
-target_pose = Odometry()
-target_distance = 0
-actuator_vel = 15
-Ianterior = 0
-rate_value = 10
-Kp = 1 
-Ki = 0 
-result = Float64()
-result.data = 0
-windDir= Float64()
-windDir.data = 1.5 
-currentHeading= Float64()
-currentHeading.data = 0
-f_distance = 4 
-sail_min = 0 
-sail_max = math.pi/2
-current_heading = 0
-rudder_min = -math.pi/3
-rudder_max = math.pi/3
-rudder_med = (rudder_min + rudder_max)/2
-heeling = 0
-spHeading = 10 
-isTacking = 0
+data = {
+    'pose': None,
+    'state': None,
+    'map_bounds': None,
+    'wind': None,
+}
+subscribtions = {}
 
-def get_pose(initial_pose_tmp):
-    global initial_pose 
-    initial_pose = initial_pose_tmp
+def show_stats(func):
+    stats = {'count': 0, 'first_call_time': None, 'duration': 0}
+    def wrapper(*args, **kwargs):
+        if stats['first_call_time'] is None:
+            stats['first_call_time'] = time.time()
+        stats['count'] += 1
+        t0 = time.time()
+        res = func(*args, **kwargs)
+        t1 = time.time()
+        stats['duration'] += t1 - t0
 
-def get_target(target_pose_tmp):
-    global target_pose 
-    target_pose = target_pose_tmp
-    
-def sail_ctrl_msg():
-    global actuator_vel
-    msg = JointState()
-    msg.header = Header()
-    msg.name = ['fwd']
-    msg.position = [sail_ctrl()]
-    msg.velocity = []
-    msg.effort = []
-    return msg
+        freq = stats['count'] / (time.time() - stats['first_call_time'])
+        mean_duration = stats['duration'] / stats['count']
+        print('[GYM_SERVER] [%s] freq: %f/s, duration: ~%fms' % (func.__name__, freq, mean_duration / 1000))
+    return wrapper
 
-def verify_result():
-    global target_distance
-    global result
-    global f_distance
-    if target_distance < f_distance:
-        result.data = 1
-    if target_distance >= f_distance:    
-        result.data = 0
-    return result
+# @show_stats
+def pose_cb(msg):
+    data['pose'] = msg
 
-def angle_saturation(sensor):
-    if sensor > 180:
-        sensor = sensor - 360
-    if sensor < -180:
-        sensor = sensor + 360
-    return sensor
+# @show_stats
+def state_cb(msg):
+    data['state'] = msg
 
-def talker_ctrl():
-    global rate_value
-    global currentHeading
-    global windDir 
-    global isTacking
-    global heeling
-    global spHeading
+def init_states_cb(msg):
+    names = msg.name
+    min_x = min_y = min_z = float('inf')
+    max_x = max_y = max_z = float('-inf')
+    for i, name in enumerate(names):
+        # the map bounds are defined by the buoys 
+        if 'buoy' in name:
+            pos = msg.pose[i].position
+            min_x = min(min_x, pos.x)
+            min_y = min(min_y, pos.y)
+            min_z = min(min_z, pos.z)
+            max_x = max(max_x, pos.x)
+            max_y = max(max_y, pos.y)
+            max_z = max(max_z, pos.z)
+    data['map_bounds'] = {
+        'min_position': {'x': min_x, 'y': min_y, 'z': min_z},
+        'max_position': {'x': max_x, 'y': max_y, 'z': max_z},
+    }
+    subscribtions['init_states'].unregister()
 
+def get_sensors():
+    position = data['pose'].pose.pose.position
+    orientation = data['pose'].pose.pose.orientation
+    linear_vel = data['pose'].twist.twist.linear
+    angular_vel = data['pose'].twist.twist.angular
+
+    heading_euler = tf.transformations.euler_from_quaternion(
+        (orientation.x, orientation.y, orientation.z, orientation.w)
+    )
+
+    return {
+        'p_boat': {'x': position.x, 'y': position.y, 'z': position.z},
+        'dt_p_boat': {'x': linear_vel.x, 'y': linear_vel.y, 'z': linear_vel.z},
+
+        'theta_boat': {'x': heading_euler[0], 'y': heading_euler[1], 'z': heading_euler[2]},
+        'dt_theta_boat': {'x': angular_vel.x, 'y': angular_vel.y, 'z': angular_vel.z},
+
+        'theta_rudder': data['state'].position[0],
+        'dt_theta_rudder': data['state'].velocity[0],
+
+        'theta_sail': data['state'].position[1],
+        'dt_theta_sail': data['state'].velocity[1],
+
+        'wind': {'x': data['wind']['x'], 'y': data['wind']['y']},
+    }
+
+def get_info():
+    return {}
+
+def get_init_info():
+    while data['map_bounds'] is None:
+        print('[GYM_SERVER] waiting for map bounds...')
+        time.sleep(0.1)
+    return {
+        'min_position': data['map_bounds']['min_position'],
+        'max_position': data['map_bounds']['max_position'],
+    }
+
+def convert_act_to_instr(act):
+    theta_rudder = act['theta_rudder']
+    theta_sail = act['theta_sail']
+    assert -math.pi <= theta_rudder <= math.pi, 'theta_rudder must be in [-pi, pi]'
+    assert -math.pi <= theta_sail <= math.pi, 'theta_sail must be in [-pi, pi]'
+    instr = JointState()
+    instr.header = Header()
+    instr.name = ['rudder_joint', 'sail_joint']
+    instr.position = [theta_rudder, theta_sail]
+    instr.velocity = []
+    instr.effort = []
+    return instr
+
+def create_zmq_socket():
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind('tcp://*:5555')
+    print('[GYM_SERVER] zmq socket listening on port 5555')
+    return socket
+
+def handle_wind_current(req):
+    # for now we use a global wind
+    assert data['wind'] is not None, 'wind is not initialized'
+    return GetSpeedResponse(data['wind']['x'], data['wind']['y'], 0)
+
+def is_ready():
+    return data['pose'] is not None \
+        and data['state'] is not None \
+        and data['map_bounds'] is not None
+
+def pause_simulation():
+    rospy.ServiceProxy('/gazebo/pause_physics', Empty)()
+
+def resume_simulation():
+    rospy.ServiceProxy('/gazebo/unpause_physics', Empty)()
+
+def reset_world():
+    rospy.ServiceProxy('/gazebo/pause_physics', Empty)()
+    rospy.ServiceProxy('/gazebo/reset_world', Empty)()
+    rospy.ServiceProxy('/gazebo/unpause_physics', Empty)()
+
+def reset_simulation():
+    # reset simulation
+    reset_world()
+
+    data['pose'] = None
+    data['state'] = None
+    while not is_ready():
+        print('[GYM_SERVER] preparing simulation...')
+        time.sleep(0.001)
+
+def run_worker():
     rospy.init_node('usv_simple_ctrl', anonymous=True)
-    rate = rospy.Rate(rate_value) # 10h
-    # publishes to thruster and rudder topics
-    #pub_sail = rospy.Publisher('angleLimits', Float64, queue_size=10)
+    socket = create_zmq_socket()
+
+    # zmq helpers
+    def send_msg(msg):
+        socket.send(msgpack.packb(msg, use_bin_type=False))
+    def recv_msg():
+        return msgpack.unpackb(socket.recv(), raw=True)
+
+    # publishers
     pub_rudder = rospy.Publisher('joint_setpoint', JointState, queue_size=10)
-    pub_result = rospy.Publisher('move_usv/result', Float64, queue_size=10)
-    pub_heading = rospy.Publisher('currentHeading', Float64, queue_size=10)
-    pub_windDir = rospy.Publisher('windDirection', Float64, queue_size=10)
-    pub_heeling = rospy.Publisher('heeling', Float64, queue_size=10)
-    pub_spHeading = rospy.Publisher('spHeading', Float64, queue_size=10)
-    
-    # subscribe to state and targer point topics
-    rospy.Subscriber("state", Odometry, get_pose)  # get usv position (add 'gps' position latter)
-    rospy.Subscriber("move_usv/goal", Odometry, get_target)  # get target position
+
+    # subscribers
+    subscribtions['pose'] = rospy.Subscriber('state', Odometry, pose_cb)
+    subscribtions['state'] = rospy.Subscriber('joint_states', JointState, state_cb)
+    subscribtions['init_states'] = rospy.Subscriber('/gazebo/model_states', ModelStates, init_states_cb)
+
+    # services
+    rospy.Service('/windCurrent', GetSpeed, handle_wind_current)
+
+    # prepare services
+    rospy.wait_for_service('/gazebo/unpause_physics')
+    rospy.wait_for_service('/gazebo/pause_physics')
+    rospy.wait_for_service('/gazebo/reset_world')
+
+    # rate
+    rate = None
 
     while not rospy.is_shutdown():
         try:
-            pub_rudder.publish(rudder_ctrl_msg())
-            #pub_sail.publish(sail_ctrl())
-            pub_result.publish(verify_result())
-            pub_heading.publish(currentHeading)
-            pub_windDir.publish(windDir)
-            pub_heeling.publish(heeling)
-            pub_spHeading.publish(spHeading)
-            rate.sleep()
+            msg = recv_msg()
+            try:
+                if 'reset' in msg:
+                    data['wind'] = msg['reset']['wind']
+                    rate = rospy.Rate(msg['reset']['freq'])
+                    reset_simulation()
+                    send_msg({'obs': get_sensors(), 'done': False, 'info': get_init_info()})
+                elif 'action' in msg:
+                    instr = convert_act_to_instr(msg['action'])
+                    pub_rudder.publish(instr)
+                    rate.sleep()
+                    send_msg({'obs': get_sensors(), 'done': False, 'info': get_info()})
+                elif 'close' in msg:
+                    pause_simulation()
+                    send_msg({'obs': None, 'done': True, 'info': get_info()})
+                else:
+                    raise AssertionError('Unknown message: %s' % str(msg))
+            except AssertionError as e:
+                print('[GYM_SERVER] Error: %s' % str(e))
+                send_msg({'error': str(e)})
         except rospy.ROSInterruptException:
-	    rospy.logerr("ROS Interrupt Exception! Just ignore the exception!")
+            rospy.logerr('ROS Interrupt Exception! Just ignore the exception!')
         except rospy.ROSTimeMovedBackwardsException:
-	    rospy.logerr("ROS Time Backwards! Just ignore the exception!")
+            rospy.logerr('ROS Time Backwards! Just ignore the exception!')
 
-def P(erro):
-    global Kp
-    return Kp * erro
-
-def I(erro):
-    global Ki
-    global Ianterior
-    global rate_value
-    if (Ianterior > 0 and erro < 0) or (Ianterior < 0 and erro > 0):
-        Ianterior = Ianterior + Ki * erro * 50 * (1./rate_value)
-    else:
-        Ianterior = Ianterior + Ki * erro * (1./rate_value)
-    return Ianterior
-
-def sail_ctrl():
-    global current_heading
-    global windDir
-    global heeling
-    # receber posicaoo do vento (no ref do veleiro)
-    x = rospy.get_param('/uwsim/wind/x')
-    y = rospy.get_param('/uwsim/wind/y')
-    global_dir = math.atan2(y,x)
-    heeling = angle_saturation(math.degrees(global_dir)+180)
-    #rospy.loginfo("valor de wind_dir = %f", math.degrees(windDir))
-    #rospy.loginfo("global_dir = %f", math.degrees(global_dir))
-    #rospy.loginfo("current_heading = %f", math.degrees(current_heading))
-    wind_dir = global_dir - current_heading
-    wind_dir = angle_saturation(math.degrees(wind_dir)+180)
-    windDir.data = math.radians(wind_dir)
-
-    #rospy.loginfo("wind_dir = %f", wind_dir)
-    #rospy.loginfo("valor de pi/2 = %f", math.pi/2)
-    #rospy.loginfo("valor de wind_dir/pi/2 = %f", wind_dir/math.pi/2)
-    #rospy.loginfo("valor de sail_max - sail_min = %f", sail_max - sail_min)
-    #rospy.loginfo("valor de (sail_max - sail_min) * (wind_dir/(math.pi/2)) = %f", (sail_max - sail_min) * (wind_dir/(math.pi/2)))
-    #rospy.loginfo("valor de sail_min = %f", sail_min)
-    #sail_angle = sail_min + (sail_max - sail_min) * (wind_dir/180)
-    
-    sail_angle = math.radians(wind_dir)/2;
-    if math.degrees(sail_angle) < -80:
-        sail_angle = -sail_angle
-    #if sail_angle < 0:
-    #    sail_angle = -sail_angle
-#    rospy.loginfo("sail angle = %f", math.degrees(sail_angle))
-    return -sail_angle
-
-def rudder_ctrl():
-    # erro = sp - atual
-    # ver qual gira no horario ou anti-horario
-    # aciona o motor (por enquanto valor fixo)
-    global initial_pose
-    global target_pose
-    global target_distance
-    global actuator_vel
-    global Ianterior
-    global rate_value
-    global current_heading
-    global currentHeading
-    global spHeading
-
-    x1 = initial_pose.pose.pose.position.x
-    y1 = initial_pose.pose.pose.position.y
-    x2 = target_pose.pose.pose.position.x
-    y2 = target_pose.pose.pose.position.y
-
-    # encontra angulo ate o ponto de destino (sp)
-    myradians = math.atan2(y2-y1,x2-x1)
-    sp_angle = math.degrees(myradians)
-
-    target_distance = math.hypot(x2-x1, y2-y1) 
-    #rospy.loginfo("target_distance: %f", target_distance)
-
-    # encontra angulo atual
-    # initial_pose.pose.pose.orientation = nav_msgs.msg.Quaternion(*tf_conversions.transformations.quaternion_from_euler(roll, pitch, yaw))
-    
-    quaternion = (initial_pose.pose.pose.orientation.x, initial_pose.pose.pose.orientation.y, initial_pose.pose.pose.orientation.z,initial_pose.pose.pose.orientation.w) 
-    euler = tf.transformations.euler_from_quaternion(quaternion) 
-
-    # target_angle = initial_pose.pose.pose.orientation.yaw
-    target_angle = math.degrees(euler[2])
-    #rospy.loginfo("target_angle %f", target_angle)
-
-#    rospy.loginfo(sp_angle)
-    sp_angle = angle_saturation(sp_angle)
-    spHeading = sp_angle
-    sp_angle = -sp_angle
-#    rospy.loginfo("sp: %f", sp_angle)
-    target_angle = angle_saturation(target_angle)
-    target_angle = -target_angle
-#    rospy.loginfo("target_angle: %f", target_angle)
-
-    current_heading = math.radians(target_angle)
-    currentHeading.data = current_heading
-    
-    err = sp_angle - target_angle
-    err = angle_saturation(err)
-    err = P(err) + I(err)
-
-    rudder_angle = err/2
-
-    if err > 60:
-        err = 60
-    if err < -60:
-        err = -60
-
-#    if err > 180:
-#        err = 180
-#    if err < -180:
-#        err = -180
-#
-#    if err < 0:
-#        rudder_angle = rudder_med + (rudder_min - rudder_med) * ((err) / 180)
-#    else:
-#        rudder_angle = rudder_med + (rudder_max - rudder_med) * (-err / 180)
-
-    log_msg = "sp: {0}; erro: {1}; x_atual: {2}; y_atual: {3}; x_destino: {4}; y_destino: {5}; distancia_destino: {6}, rudder_angle: {7}; target_angle: {8}" .format(sp_angle, err, initial_pose.pose.pose.position.x, initial_pose.pose.pose.position.y, target_pose.pose.pose.position.x, target_pose.pose.pose.position.y, target_distance, rudder_angle, target_angle)
-
-    #rospy.loginfo(log_msg)
-
-    return math.radians(rudder_angle)
-
-def rudder_ctrl_msg():
-    msg = JointState()
-    msg.header = Header()
-    msg.name = ['rudder_joint', 'sail_joint']
-    msg.position = [rudder_ctrl(), sail_ctrl()]
-    msg.velocity = []
-    msg.effort = []
-    return msg
+    # send final message
+    recv_msg()
+    send_msg({'obs': None, 'done': True, 'info': None})
+    pause_simulation()
 
 if __name__ == '__main__':
-    try:
-        talker_ctrl()
-    except rospy.ROSInterruptException:
-        pass
+    run_worker()
