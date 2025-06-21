cafb77cdd0fdcc138a61f408a663670cad7a0b5a
Chris Nassif <chrisjnassif@gmail.com>
2025-06-21 12:06:26 -0400

    fixed the build_sim.sh to build an image with the name of aanimated/sailbot_simulation instead of the old name

--- build_sim.sh
+++ build_sim.sh
@@ -1,4 +1,4 @@
-docker buildx build --build-arg max_step_size=0.001 --build-arg physics_type=ode --tag lucasmrdt/sailboat-sim-lsa-gym:mss1-ode --cache-from type=registry,ref=lucasmrdt/sailboat-sim-lsa-gym .
+docker buildx build --build-arg max_step_size=0.001 --build-arg physics_type=ode --tag aanimated/sailbot_simulation --cache-from type=registry,ref=lucasmrdt/sailboat-sim-lsa-gym .
 
 # https://www.squash.io/preventing-terminal-print-from-bash-scripts-in-linux/#:~:text=One%20way%20to%20suppress%20the,%2Fdev%2Fnull%E2%80%9D).
 docker container stop sailboat-sim-lsa-gym-default > /dev/null 2>&1
